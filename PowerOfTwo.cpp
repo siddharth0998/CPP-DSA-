b215f09030b056afed414905f61f154647e046c4
Siddharth Patel <105500935+siddharth0998@users.noreply.github.com>
2022-09-28 00:15:27 +0530

    commit

--- PowerOfTwo.cpp
+++ PowerOfTwo.cpp
@@ -9,8 +9,19 @@ public:
         //     }
         // }
         // return false;
+
+        // solution 2
+        //int ans = 1;
+        // for(int i=0; i<=30; i++){
+        //     if(n == ans) {
+        //         return true;
+        //     }
+        //     if(ans < INT_MAX/2)
+        //     ans = ans*2;
+        // }
+        // return false;
         
-        // faster solution 2
+        // faster solution 3
         if(n<=0) return false;
         return ((n&n-1)==0);
     }
